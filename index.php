efb6d5b0ee43989a724144474c1e0277f4d6e3d5
s3725150 <s3725150@student.rmit.edu.au>
2020-05-13 22:31:44 +1000

    added stuff for GAE deployment

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
-<!DOCTYPE html>
-<html>
-<body>
-<div> Here try using my steamId - 76561198038149325 </div>
-<form action="/search.php" method = "post">
-  <label for="steamId">Steam ID:</label><br>
-  <input type="text" id="steamId" name="steamId"><br>
-  <input type="submit" value="Submit">
-</form>
-</body>
-</html>
+<?php
+/**
+ * This is an example of a front controller for a flat file PHP site. Using a
+ * Static list provides security against URL injection by default.
+ */
+# [START gae_simple_front_controller]
+switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
+    case '/':
+        require 'login.php';
+        break;
+    case '/search.php':
+        require 'search.php';
+        break;
+    default:
+        http_response_code(404);
+        exit('Not Found');
+}
+# [END gae_simple_front_controller]
+?>
