6053a6215501d69b3ee7eec731305ecdaf8749a0
s3725150 <s3725150@student.rmit.edu.au>
2020-05-15 02:16:42 +1000

    stat test

--- stats.php
+++ stats.php
@@ -6,18 +6,30 @@
 <body>
 <h1> Steam Chat </h1>
 <?php
-echo $_POST['steamId'];
 
 $postRequest = array(
     'steamId' => $_POST['steamId']
 );
 
-$cURLConnection = curl_init('35.239.38.150/addUser');
+$cURLConnection = curl_init('localhost:5000/addUser');
 curl_setopt($cURLConnection, CURLOPT_POSTFIELDS, $postRequest);
 curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
 
 $apiResponse = curl_exec($cURLConnection);
 curl_close($cURLConnection);
+
+
+//Sort through the data
+$parsed_json = json_decode($apiResponse, true);
+echo '<br>';
+echo $parsed_json['name'];
+echo '<br>';
+echo 'Playtime in hours: ';
+echo $parsed_json['playtime'];
+echo '<br>';
+echo '<div>';
+echo '<img style="display:inline-block;margin: 1px;" src='. $parsed_json['avatar'] . '>'; 
+echo '</div>';
 ?>
 </body>
 </html>
