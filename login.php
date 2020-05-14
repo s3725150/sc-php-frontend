fbc7e4f258400eb4b0a5279684396d9a7bd728cf
s3725150 <s3725150@student.rmit.edu.au>
2020-05-14 18:06:38 +1000

    bugfix

--- login.php
+++ login.php
@@ -6,12 +6,12 @@
 <body>
 <h1> Steam Chat </h1>
 <div> Hi! Here try using my steamId - 76561198038149325 </div>
-<form id = loginform>
+<form id = loginform action="/search.php" method="post">
   <label for="steamId">Steam ID:</label><br>
   http://steamcommunity.com/profiles/
   <input type="text" id="steamId" name="steamId" required><br>
-  <input type="submit" class = "button" value="Chat About Games!" formaction="/search.php" method = "post">
-  <input type="submit" class = "button"value="Wasted on Steam?" formaction="/stats.php" method = "post">
+  <input type="submit" class = "button" value="Chat About Games!">
+  <input type="submit" class = "button"value="Wasted on Steam?" formaction="/stats.php">
 </form>
 </body>
 </html>
